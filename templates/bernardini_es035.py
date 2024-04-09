a64288be634aea3e532f8ee68e6fb41ab285e229
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-04-09 11:35:37 +0200

    list cities

--- templates/bernardini_es035.py
+++ templates/bernardini_es035.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "<p>Welcome to our Weather Application!</p>"
+
+@app.route("/cities")
+def cities():
+    return render_template("list.html")
