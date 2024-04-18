b4bd94987aae9cd7a25a3d326f0c79d8e8aa4481
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-04-18 13:29:42 +0200

    .

--- bernardini_es037.py
+++ bernardini_es037.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
-
+@app.route("/")
 def index():
     with open("bernardini_es037_list.json") as f:
         data = json.load(f)
-    return render_template("bernardini_es037_route.html", links=data)
-
-index()
+    return render_template("bernardini_es037_route.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=1432)
