20b46253bc9a3011122ee1836bccb17f041d5a55
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-05-02 13:35:14 +0200

    es 39 incompleto

--- bernardini_es039.py
+++ bernardini_es039.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 import json
 
-app = Flask(__name__)
-
-@app.route("/flashcard/<id>")
-def flashcards(id):
+def get_flashcard_by_id(id: int) -> None:
     with open("bernardini_es039_list.json", "r") as f:
-        
-    return render_template("bernardini_es039_route.html")
+        flashcards = json.load(f)
+        for i in flashcards:
+            if i["id"] == id:
+                return i
+
+
+def prompt_for_id() -> int:
+    id = int(input("Inserisci un ID da 1 a 3: "))
+    flashcard = get_flashcard_by_id(id)
+    print(flashcard["question"])
+
+def prompt_for_answer() -> str:
+    risposta = str(input("Risposta: "))
+    return risposta
+
+def check_answer(risposta: str, risposta_corretta: str) -> bool:
+    return risposta.lower == risposta_corretta.lower()
+
+prompt_for_id()
+risposta = prompt_for_answer()
