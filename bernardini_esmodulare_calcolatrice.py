7cfd7bcf9641754c6b9c45e4d4dea90b4ecb1401
LucaBernardinii <luca.bernardini@studenti.isissgobetti.it>
2024-05-14 11:14:34 +0200

    modulazione

---
+++ bernardini_esmodulare_calcolatrice.py
@@ -0,0 +1,12 @@
+def inserisci_numeri() -> list:
+    pass
+
+def scegli_operazione() -> str:
+    pass
+
+def calcola(num1: float, num2: float, operazione: str) -> float:
+    pass
+
+def main() -> float:
+    risultato = calcola
+    print(risultato)
